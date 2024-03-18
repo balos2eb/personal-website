30aaf8bcf723132c794dc9737b92f8865d916b30
balos2eb <balos2eb@gmail.com>
2024-03-17 23:40:37 -0700

    Added loaders to the home and about pages (#8)

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,4 @@
+import Loader from 'react-loaders';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react'
 import AnimatedLetters from '../AnimatedLetters';
@@ -5,7 +6,7 @@ import './index.scss';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
-    const nameArray = [' ', 'E', 'm', 'e', 'r', 's', 'o', 'n'];
+    const nameArray = [' ', 'E', 'm', 'e', 'r', 's', 'o', 'n', ','];
     const jobArray = ['s', 't', 'u', 'd', 'e', 'n', 't', '.'];
 
     const hoverLetter = async () => {
@@ -19,26 +20,29 @@ const Home = () => {
     }, [])
 
     return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                    <span className={letterClass}>H</span>
-                    <span className={`${letterClass} _12`}>i,</span>
-                    <br />
-                    <span className={`${letterClass} _13`}>I</span>
-                    <span className={`${letterClass} _14`}>'m</span>
-                    <AnimatedLetters letterClass={letterClass}
-                        strArray={nameArray}
-                        index={15} />
-                    <br />
-                    <AnimatedLetters letterClass={letterClass}
-                        strArray={jobArray}
-                        index={22} />
-                </h1>
-                <h2>Student @ Langara College / Game Master @ Zero Latency</h2>
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i,</span>
+                        <br />
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
+                        <AnimatedLetters letterClass={letterClass}
+                            strArray={nameArray}
+                            index={15} />
+                        <br />
+                        <AnimatedLetters letterClass={letterClass}
+                            strArray={jobArray}
+                            index={24} />
+                    </h1>
+                    <h2>Student @ Langara College / Game Master @ Zero Latency</h2>
+                    <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+                </div>
             </div>
-        </div>
+            <Loader type="pacman" />
+        </>
     )
 };
 
